48f6c6e1748c43f331bd14fe26dbcd9b52df497c
M. Oğuzhan Ataman <moguzhanataman@gmail.com>
2024-09-20 13:38:03 +0300

    Add class-validator, Add DTO validation to register.dto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class RegisterDto {
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   age: number;
 }
--- src/errors/http-exception.filter.ts
+++ src/errors/http-exception.filter.ts
@@ -26,11 +26,6 @@ export class HttpExceptionFilter implements ExceptionFilter {
         ? exception.getResponse()
         : 'Internal server error';
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message,
-    });
+    response.status(status).json(message);
   }
 }
