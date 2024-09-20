1d70c2617a3a73c269b56f3f7cc849357ed5ded1
M. Oğuzhan Ataman <moguzhanataman@gmail.com>
2024-09-20 13:35:33 +0300

    Add DTOs for login, register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthService) {}
   @Post('login')
-  login() {
+  login(@Body() loginDto: LoginDto) {
     return 'ok';
   }
 
   @Post('register')
-  async register() {
-    await this.authService.registerUser('oguzhan', '123', 18);
+  async register(@Body() registerDto: RegisterDto) {
+    await this.authService.registerUser(
+      registerDto.username,
+      registerDto.password,
+      registerDto.age,
+    );
   }
 }
