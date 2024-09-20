c0d78dab70fcf2d63c25b52630024149dd302179
M. Oğuzhan Ataman <moguzhanataman@gmail.com>
2024-09-20 14:08:47 +0300

    Move token expire time to .env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRE },
     }),
   ],
   controllers: [AuthenticationController],
