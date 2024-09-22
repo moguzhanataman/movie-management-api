bc75e9d49a9a0fd40ad22fdc88e38068e6c1e590
M. Oğuzhan Ataman <moguzhanataman@gmail.com>
2024-09-22 23:43:10 +0300

    Setup customer guard for ticket/buy

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { BuyTicketDto } from './dto/buy-ticket.dto';
 import { TicketService } from './ticket.service';
+import { CustomerGuard } from 'src/auth/customer.guard';
 
 @Controller('ticket')
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
+  @UseGuards(CustomerGuard)
   @Post('buy')
   async buyTicket(@Body() buyTicketDto: BuyTicketDto) {
     await this.ticketService.buyTicket(1, buyTicketDto.movieSessionId);
